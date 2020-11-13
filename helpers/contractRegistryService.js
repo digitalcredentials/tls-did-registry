ede95caaecdb2e80919a794b91474df682ff4026
Kilian Käslin <kilian.kaeslin@tum.de>
2020-11-13 13:07:12 +0100

    - Updated helper function

--- helpers/contractRegistryService.js
+++ helpers/contractRegistryService.js
@@ -1,13 +1,7 @@
 const fs = require('fs');
 
 exports.storeRegistryAddress = function storeRegistryAddress(address) {
-  const readData = fs.readFileSync(
-    `${__dirname}/contractRegistry.json`,
-    'utf8'
-  );
-  let contractRegistry = JSON.parse(readData);
-
-  contractRegistry.registryAddress = address;
+  const contractRegistry = { registryAddress: address };
 
   const writeData = JSON.stringify(contractRegistry);
   fs.writeFileSync(`${__dirname}/contractRegistry.json`, writeData);
